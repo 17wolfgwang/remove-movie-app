3f0d070b7a5a9c4ac53066320e35a810cbc6dea6
17wolfgwang <dkzks44@gmail.com>
2023-09-25 03:58:49 +0900

    Create Project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from "../core/heropy";
+
+export default new Store({
+    photo: 'https://heropy.blog/css/images/logo.png',
+    name: '17wolfgwang / Stan Jang',
+    email: 'dkzks44@gmail.com',
+    blog: 'https://heropy.blog',
+    github: 'https://github.com/17wolfgwang',
+    repository: 'https://github.com/17wolfgwang'
+})
